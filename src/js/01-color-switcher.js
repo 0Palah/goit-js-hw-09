e80f699484e3e57f3a9ef0f43bf27e68fe4e3de9
Oleg Palashynskyi <palah@ukr.net>
2022-09-08 15:23:39 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStotEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+btnStartEl.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+console.log(bodyEl);

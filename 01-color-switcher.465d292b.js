4b204dc9eaed02510e9c1ea0f64486d089f4aedd
0Palah <106759871+0Palah@users.noreply.github.com>
2022-09-08 12:24:21 +0000

    Deploying to gh-pages from @ 0Palah/goit-js-hw-09@e80f699484e3e57f3a9ef0f43bf27e68fe4e3de9 🚀

---
+++ 01-color-switcher.465d292b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=(document.querySelector("button[data-stop]"),document.querySelector("body"));t.addEventListener("click",(function(){o.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))})),console.log(o)}();
+//# sourceMappingURL=01-color-switcher.465d292b.js.map

1a76a818a8cb0b24f14ed25d46649903f97d9cce
Oleg Palashynskyi <palah@ukr.net>
2022-09-08 17:13:37 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,26 @@
 const btnStartEl = document.querySelector('button[data-start]');
-const btnStotEl = document.querySelector('button[data-stop]');
-const bodyEl = document.querySelector('body');
-
-btnStartEl.addEventListener('click', onBtnClick);
-
-function onBtnClick() {
-  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
-}
+const btnStopEl = document.querySelector('button[data-stop]');
+let bodyEl = document.querySelector('body');
+let chengeColorId = null;
+btnStopEl.disabled = true;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+btnStartEl.addEventListener('click', () => {
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  chengeColorId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStopEl.addEventListener('click', () => {
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+  clearInterval(chengeColorId);
+});
 
 console.log(bodyEl);
